7eb6f30dc6ace403effff6b07c740198d9f6899f
= <alexandressoares383@gmail.com>
2022-07-09 18:09:18 -0300

    feat: snapshot

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -2,17 +2,46 @@ import { mount } from '@vue/test-utils';
 
 import ProductCard from './ProductCard';
 
+import { makeServer } from '@/miragejs/server';
+
 describe('ProductCard - unit', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({
+      environment: 'test',
+    });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should macth snapchot', () => {
+    const wrapper = mount(ProductCard, {
+      propsData: {
+        product: server.create('product', {
+          title: 'saepe quae et',
+          price: '$782.00',
+        }),
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapchot();
+  });
+
   it('should mount the component', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'saepe quae et',
+          price: '$782.00',
+        }),
       },
     });
 
     expect(wrapper.vm).toBeDefined();
-
-    // eslint-disable-next-line no-console
-    console.log(wrapper.classes());
+    expect(wrapper.text()).toContain('saepe quae et');
+    expect(wrapper.text()).toContain('$782.00');
   });
 });
